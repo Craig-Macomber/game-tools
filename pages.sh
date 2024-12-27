51d3b3d2a86464a781014111c9aab7cd2fdff580
Craig Macomber <CraigM@CraigM.info>
2024-12-26 17:58:41 -0800

    Try top level index with relative links

--- pages.sh
+++ pages.sh
@@ -30,11 +30,13 @@ cd ..
 git branch -d pages
 git checkout -b pages
 
-cp -r ./target/dx/roll/release/web/public/* .
+git add -f ./target/dx/roll/release/web/public
 
-git add -f index.html
-git add -f assets
-git add -f wasm
+# cp -r ./target/dx/roll/release/web/public/* .
+
+# git add -f index.html
+# git add -f assets
+# git add -f wasm
 
 git commit -m "Web build for pages"
 git push -f --set-upstream origin pages
